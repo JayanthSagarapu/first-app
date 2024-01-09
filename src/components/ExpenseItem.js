b120c96162528fb1d7c68ce601998dc4fff869b5
JayanthSagarapu <sjayanthkumar1@gmail.com>
2024-01-09 15:14:48 +0530

    expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,17 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseTitle = "Food";
+  const expenseAmount = 100;
+  const expenseLocation = "Vizag";
+
   return (
-    <div>
-      <h2>Expense Item!</h2>
-      <div>
-        <h3>Food Rs 10</h3>
-        <h3>Petrol Rs 100</h3>
-        <h3>Movies Rs 200</h3>
+    <div className="expense-item">
+      <div>Expense Item!!</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{expenseLocation}</h2>
+        <div className="expense-item__price">Rs{expenseAmount}</div>
       </div>
     </div>
   );
